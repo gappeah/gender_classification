357d6e75e7eaec74d1a2fd203e6295021d991a30
George <appeahg@outlook.com>
2024-04-09 16:46:25 +0100

    Commit to main

--- demo.py
+++ demo.py
@@ -1,1 +1,25 @@
+#This is the simple data science project for the gender classification
+
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+
+clf = tree.DecisionTreeClassifier()
+
+#create 3 more classifiers...
+
+
+
+
+# [height, weight, shoe_size]
+
+x = [[181, 80, 9], [177, 70, 8], [160, 60, 2], [154, 54, 5], [166, 65, 7], [190, 90, 8], [175, 64, 6]]
+
+y = ['male', 'male', 'female', 'female', 'male', 'male', 'female']
+
+
+clf = clf.fit(x,y)
+
+prediction = clf.predict([[190, 70, 8]])
+
+print(prediction)
 
